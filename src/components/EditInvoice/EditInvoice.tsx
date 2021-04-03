3a35982c18eb547b957ed9c76985263af8e88983
JakubKepak <32369029+JakubKepak@users.noreply.github.com>
2021-04-03 20:51:47 +0200

    update formik validation

--- src/components/EditInvoice/EditInvoice.tsx
+++ src/components/EditInvoice/EditInvoice.tsx
@@ -10,19 +10,18 @@ import deleteIcon from "../../assets/icon-delete.svg";
 
 const EditSchema = Yup.object().shape({
   providerStreetAddress: Yup.string().required("Required"),
-  // providerCity: Yup.string().required("Required"),
-  // providerPostalCode: Yup.string().required("Required"),
-  // providerCountry: Yup.string().required("Required"),
-  // clientsName: Yup.string().required("Required").min(2, "Too Short!"),
-  // clientsEmail: Yup.string().required("Required"),
-  // clientsStreetAddress: Yup.string().required("Required"),
-  // clientCity: Yup.string().required("Required"),
-  // clientPostalCode: Yup.string().required("Required"),
-  // clientCountry: Yup.string().required("Required"),
-  // invoiceDate: Yup.string().required("Required"),
-  // paymentTerms: Yup.string().required("Required"),
-  // description: Yup.string().required("Required"),
-  // invoiceItems: Yup.string().required("Required"),
+  providerCity: Yup.string().required("Required"),
+  providerPostalCode: Yup.string().required("Required"),
+  providerCountry: Yup.string().required("Required"),
+  clientsName: Yup.string().required("Required").min(2, "Too Short!"),
+  clientsEmail: Yup.string().required("Required"),
+  clientsStreetAddress: Yup.string().required("Required"),
+  clientCity: Yup.string().required("Required"),
+  clientPostalCode: Yup.string().required("Required"),
+  clientCountry: Yup.string().required("Required"),
+  invoiceDate: Yup.string().required("Required"),
+  paymentTerms: Yup.string().required("Required"),
+  description: Yup.string().required("Required"),
 });
 
 const CustomTextField = ({ label, ...props }: any) => {
