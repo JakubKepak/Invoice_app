5c728f408ad150fbe46dfe349bcd34d496783f21
JakubKepak <32369029+JakubKepak@users.noreply.github.com>
2021-04-04 11:48:42 +0200

    add custom hook boilerplate for managing state

--- src/hooks/useDBHandler.tsx
+++ src/hooks/useDBHandler.tsx
@@ -1,3 +1,14 @@
+import { useEffect, useReducer } from "react";
+import { reducer } from "../reducers/reducer";
+
 export default function useDBHandler() {
+  const initialState = {
+    status: "loading",
+    data: undefined,
+    error: null,
+  };
+
+  const [state, dispatch] = useReducer(reducer, initialState);
+
   return <div></div>;
 }
---
+++ src/reducers/reducer.ts
@@ -0,0 +1,17 @@
+type Action =
+  | { type: "loading" }
+  | { type: "error"; payload: any }
+  | { type: "success"; payload: any };
+
+export const reducer = (state: any, action: Action) => {
+  switch (action.type) {
+    case "loading":
+      return { status: "loading", data: undefined, error: null };
+    case "error":
+      return { status: "error", data: undefined, error: action.payload };
+    case "success":
+      return { status: "success", data: action.payload, error: null };
+    default:
+      throw new Error("invalid action");
+  }
+};
