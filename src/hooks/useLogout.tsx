2cb76b3aa1675bd42d5b2c9a87db08bc6d21fac7
JakubKepak <32369029+JakubKepak@users.noreply.github.com>
2021-04-24 10:42:18 +0200

    fix clear apollo store after logout

--- src/hooks/useLogout.tsx
+++ src/hooks/useLogout.tsx
@@ -1,10 +1,12 @@
 import { useHistory } from "react-router-dom";
+import client from "client";
 
 export default function useLogout() {
   const history = useHistory();
 
   const logout = () => {
     localStorage.removeItem("token");
+    client.clearStore();
     history.push("/login");
   };
 
