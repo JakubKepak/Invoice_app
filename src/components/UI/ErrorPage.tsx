7b45a16a9c9c0adb5c42d8dc8dcba2aa603a94e3
JakubKepak <32369029+JakubKepak@users.noreply.github.com>
2021-04-26 09:20:34 +0200

    update error page

--- src/components/UI/ErrorPage.tsx
+++ src/components/UI/ErrorPage.tsx
@@ -1,5 +1,31 @@
+import styled from "styled-components";
+import illustrationError from "assets/error.svg";
+
+const MainContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  margin-top: 20vh;
+  color: ${({ theme }) => theme.colors.mainFontColor};
+`;
+
+const ErrorImage = styled.img`
+  width: 50vw;
+`;
+
+const MessageContainer = styled.div`
+  margin-top: 3rem;
+  text-align: center;
+`;
+
 export default function ErrorPage({ error }: any) {
   console.log(error);
 
-  return <div>oh no! Something is broken.</div>;
+  return (
+    <MainContainer>
+      <ErrorImage src={illustrationError} />
+      <MessageContainer>oh no! Something is broken.</MessageContainer>
+    </MainContainer>
+  );
 }
