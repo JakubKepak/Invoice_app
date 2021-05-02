7bad65417833c2c9b071f169203a2a9aedc2d30f
JakubKepak <32369029+JakubKepak@users.noreply.github.com>
2021-05-02 21:53:29 +0200

    add filter options

--- src/components/OverviewPage/Subcomponents/Filter.tsx
+++ src/components/OverviewPage/Subcomponents/Filter.tsx
@@ -10,12 +10,33 @@ export default function Filter(): React.ReactElement {
   };
 
   return (
-    <S.FilterContainer onClick={toggleShowFilter}>
-      <span>Filter</span>
-      <S.ArrowImage src={arrowDown} showFilter={showFilter} />
-      <S.FilterOptionContainer>
-        <span>booo</span>
-      </S.FilterOptionContainer>
+    <S.FilterContainer>
+      <S.FilterButton onClick={toggleShowFilter}>
+        <span>Filter</span>
+        <S.ArrowImage src={arrowDown} showFilter={showFilter} />
+      </S.FilterButton>
+      {showFilter && (
+        <S.FilterOptionContainer>
+          <S.FilterOptionItem>
+            <S.FilterCheckbox id='filter-paid' type='checkbox' />
+            <S.FilterCheckboxLabel htmlFor='filter-paid'>
+              Paid
+            </S.FilterCheckboxLabel>
+          </S.FilterOptionItem>
+          <S.FilterOptionItem>
+            <S.FilterCheckbox id='filter-pending' type='checkbox' />
+            <S.FilterCheckboxLabel htmlFor='filter-pending'>
+              Pending
+            </S.FilterCheckboxLabel>
+          </S.FilterOptionItem>
+          <S.FilterOptionItem>
+            <S.FilterCheckbox id='filter-draft' type='checkbox' />
+            <S.FilterCheckboxLabel htmlFor='filter-draft'>
+              Draft
+            </S.FilterCheckboxLabel>
+          </S.FilterOptionItem>
+        </S.FilterOptionContainer>
+      )}
     </S.FilterContainer>
   );
 }
--- src/components/OverviewPage/Subcomponents/FilterStyles.ts
+++ src/components/OverviewPage/Subcomponents/FilterStyles.ts
@@ -2,10 +2,14 @@ import styled from 'styled-components';
 
 export const FilterContainer = styled.div`
   position: relative;
+`;
+
+export const FilterButton = styled.div`
   display: flex;
   align-items: center;
   margin-right: 1rem;
   color: ${({ theme }) => theme.colors.mainFontColor};
+  cursor: pointer;
 
   & span {
     font-weight: 700;
@@ -24,10 +28,24 @@ export const ArrowImage = styled.img<{ showFilter: boolean }>`
 `;
 
 export const FilterOptionContainer = styled.div`
+  width: 10rem;
+  left: -50%;
+  display: grid;
+  grid-gap: 0.3rem;
   position: absolute;
   top: 200%;
   background-color: ${({ theme }) => theme.colors.backgroundColorLight};
-  padding: 0.5rem;
+  padding: 1rem;
   border-radius: var(--borderRadius);
   box-shadow: var(--boxShadow);
+`;
+
+export const FilterOptionItem = styled.div``;
+
+export const FilterCheckbox = styled.input``;
+
+export const FilterCheckboxLabel = styled.label`
+  color: ${({ theme }) => theme.colors.mainFontColor};
+  font-weight: 700;
+  margin-left: 0.3rem;
 `;
