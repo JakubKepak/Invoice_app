7a6c017b18c9d86f955409bc656404518e09a89c
Pavel Pospíšil <info@pavelpo.cz>
2021-07-04 21:43:51 +0200

    MECH-13 Základní nastavení FD_Obj č. 3: Odebrání nadbytečného kódu

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,11 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+import "./App.css";
+
+export const App = () => (
+    <div className="app">
+        <header>
+            <h1>MechIS</h1>
+        </header>
     </div>
-  );
-}
-
-export default App;
+);
