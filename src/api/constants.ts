8f8b4aad434e3b72a840e5c335a1c5798992539e
Pavel Pospíšil <pavel.p.pospisil@gmail.com>
2021-10-17 23:21:37 +0200

    Updated manufacturer data URL

--- src/api/constants.ts
+++ src/api/constants.ts
@@ -1,1 +1,2 @@
-export const MANUFACTURER_LIST_URL = "https://private-anon-d9c6e1bd8e-carsapi1.apiary-mock.com/manufacturers";
+/* Temporary link for manufaturer data. It's the third party API, the URL needs to be updated due to it's expiration. */
+export const MANUFACTURER_LIST_URL = "https://private-anon-00707bb83e-carsapi1.apiary-mock.com/manufacturers";
